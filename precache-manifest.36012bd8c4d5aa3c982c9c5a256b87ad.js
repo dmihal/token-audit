d8d153651f33548761a4004726e93b2ab429e10b
David Mihal <david@dmihal.com>
2020-08-13 23:50:10 +0100

    Updates

---
+++ precache-manifest.36012bd8c4d5aa3c982c9c5a256b87ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18c89605c0cd76a6fee87f6f49beceee",
+    "url": "/token-audit/index.html"
+  },
+  {
+    "revision": "aabe05cd1372c12cfc9b",
+    "url": "/token-audit/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4f1b17d641fac1a50e7e",
+    "url": "/token-audit/static/js/2.ae2130d9.chunk.js"
+  },
+  {
+    "revision": "deea066462e3852fb7f610c3fa67aa20",
+    "url": "/token-audit/static/js/2.ae2130d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aabe05cd1372c12cfc9b",
+    "url": "/token-audit/static/js/main.435416a8.chunk.js"
+  },
+  {
+    "revision": "34d0132812b94a819252",
+    "url": "/token-audit/static/js/runtime-main.1ab6e038.js"
+  }
+]);
